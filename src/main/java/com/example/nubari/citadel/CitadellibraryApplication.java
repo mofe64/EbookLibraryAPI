d75eda700ba68c14d3135ef28bbba7815f63f276
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-02-03 13:41:00 +0100

    project setup

---
+++ src/main/java/com/example/nubari/citadel/CitadellibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.nubari.citadel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitadellibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitadellibraryApplication.class, args);
+	}
+
+}
